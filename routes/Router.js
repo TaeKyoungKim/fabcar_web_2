12b04a0c3f2950b659e7524d331a8a82e9edf252
내 이름 <you@example.com>
2020-07-08 16:25:49 +0900

    save data

--- routes/Router.js
+++ routes/Router.js
@@ -73,4 +73,15 @@ router.post('/create' ,async (req, res , next)=>{
     
     await res.redirect("/")
 })
+
+
+router.get('/detaildata/:carnum', async (req, res , next)=>{
+    console.log(req.params.carnum)
+    var searchdata = req.params.carnum
+    var result  = await queryUtil.queryData(searchdata)
+    var resultData  = await JSON.parse(result)
+    
+    console.log(resultData)
+    res.render('detaildata', {data:resultData , KEY:searchdata})
+})
 module.exports = router;
